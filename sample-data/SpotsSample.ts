26760caa00a8d4cefd80ab87476493c35617d2c6
LeoShangTang <leoshang12@gmail.com>
2024-08-07 21:04:27 -0600

    Sample data

---
+++ sample-data/SpotsSample.ts
@@ -0,0 +1,28 @@
+import forestCenter1 from "./Images/ForestCenter1.jpg"
+import forestCenter2 from "./Images/ForestCenter2.png"
+import nestBack1 from "./Images/NestBack1.png"
+import nestBack2 from "./Images/NestBack2.png"
+import nestBack3 from "./Images/NestBack3.png"
+
+const SpotsSample = [
+    {
+        id: "1",
+        name: "Nest Backside",
+        averageRating: 4.2,
+        imageLinks: [forestCenter1, forestCenter2],
+        locationLink: "",
+        ratingIds: [],
+        tags: ["washrooms"]
+    },
+    {
+        id: "2",
+        name: "Forestry Building",
+        averageRating: 4.4,
+        imageLinks: [nestBack1, nestBack2, nestBack3],   
+        locationLink: "",
+        ratingIds: [],
+        tags: ["quiet"],
+    },
+]
+
+export default SpotsSample;
