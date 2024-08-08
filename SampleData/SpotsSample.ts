03803d7525db007af244bb0e19b7eda8d8fbb94b
LeoShangTang <leoshang12@gmail.com>
2024-08-07 19:24:09 -0600

    Basic set up

---
+++ SampleData/SpotsSample.ts
@@ -0,0 +1,20 @@
+const StudySpots = [
+    {
+        id: "1",
+        name: "The Egg",
+        imageLink: "UBC_Nest_Egg.jpg",
+        locationLink: "",
+        ratingIds: [],
+        tags: ["washrooms"]
+    },
+    {
+        id: "2",
+        name: "Forestry Building",
+        imageLink: "ForestCenter.jpg",   
+        locationLink: "",
+        ratingIds: [],
+        tags: ["quiet"],
+    },
+]
+
+export default StudySpots;
