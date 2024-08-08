b8ce1dbcc27d2bc5c18276ca5c5c845fcc77f3ce
LeoShangTang <leoshang12@gmail.com>
2024-08-08 17:11:22 -0600

    Eslint styling fix
    eslint styling

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,13 +1,22 @@
-import SpotsSample from "@/sample-data/SpotsSample"
-import SpotPreview from "@/components/homepage/SpotPreview"
+import Preview from "@/components/homepage/SpotPreview";
+import sSample from "@/sample-data/SpotsSample";
 
 export default function Home() {
-    return (
-        // List of study spots
-        <div className="flex flex-row overflow-x-hidden">
-            {SpotsSample.map((spot) => 
-                (<SpotPreview  title={spot.name} location={spot.locationLink} imageLink={spot.imageLinks[0]} averageRating={spot.averageRating} totalReviews={spot.ratingIds.length} tags={spot.tags}/>))}
-        </div>
-    )
+  return (
+    // List of study spots
+    <div className="flex flex-row overflow-x-hidden">
+      {sSample.map((spot) => (
+        <Preview
+          key={spot.id}
+          title={spot.name}
+          location={spot.locationLink}
+          imageLink={spot.imageLinks[0]}
+          averageRating={spot.averageRating}
+          totalReviews={spot.ratingIds.length}
+          tags={spot.tags}
+        />
+      ))}
+    </div>
+  );
 }
 // location, imageLink averageRating
