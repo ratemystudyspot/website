3f3015227c6a51230f38575bb4995f7193b4aeb6
Leo <leoshang12@gmail.com>
2024-08-12 16:07:27 -0600

    Use link instead of router

--- src/components/homepage/SpotPreview.tsx
+++ src/components/homepage/SpotPreview.tsx
@@ -1,8 +1,6 @@
-"use client";
-
 import Image from "next/image";
 import type { StaticImageData } from "next/image";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 import RatingInfo from "./RatingInfo";
 import TagIcons from "./TagIcons";
@@ -24,31 +22,32 @@ export default function SpotPreview({
   averageRating: number;
   tags: string[];
 }) {
-  const router = useRouter();
   return (
-    <div
-      className="m-[0.8rem] flex h-[20.5rem] w-[19rem] cursor-pointer flex-col items-center rounded-[1.25rem] bg-white p-3"
-      onClick={() => router.push(`/studyspot/${id}`)}
-    >
-      <div className="h-[65%] w-full overflow-hidden rounded-[1.0625rem]">
-        <Image
-          className="size-full object-cover"
-          src={imageLink}
-          alt={title}
-          width={250}
-          placeholder="blur"
-        />
-      </div>
-      <div className="mt-2 w-full">
-        <div className="ml-1 text-[1.25rem] font-semibold text-gray-900">
-          {title}
+    <Link href={`/studyspot/${id}`}>
+      <div className="m-[0.8rem] flex h-[20.5rem] w-[19rem] cursor-pointer flex-col items-center rounded-[1.25rem] bg-white p-3">
+        <div className="h-[65%] w-full overflow-hidden rounded-[1.0625rem]">
+          <Image
+            className="size-full object-cover"
+            src={imageLink}
+            alt={title}
+            width={250}
+            placeholder="blur"
+          />
         </div>
-        <div className="ml-1 text-[1.25rem] font-semibold text-gray-900">
-          {location}
+        <div className="mt-2 w-full">
+          <div className="ml-1 text-[1.25rem] font-semibold text-gray-900">
+            {title}
+          </div>
+          <div className="ml-1 text-[1.25rem] font-semibold text-gray-900">
+            {location}
+          </div>
+          <RatingInfo
+            averageRating={averageRating}
+            totalReviews={totalReviews}
+          />
+          <TagIcons tags={tags} />
         </div>
-        <RatingInfo averageRating={averageRating} totalReviews={totalReviews} />
-        <TagIcons tags={tags} />
       </div>
-    </div>
+    </Link>
   );
 }
