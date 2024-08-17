aad3c6fc51b4a716823fb1134744c02de5ad8173
Burton Jong <burtonjong05@gmail.com>
2024-08-16 18:27:51 -0600

    style looks ok now

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,7 +11,7 @@ const peopleObject = {
     message: " yap yap yap",
   },
   Person2: {
-    name: "leo shang tang",
+    name: "leo shang",
     title: "cofounder",
     linkedin: "http://google.ca/",
     github: "http://google.ca/",
@@ -24,16 +24,19 @@ export default function Home() {
   return (
     // List of study spots
 
-    <main className="relative flex min-h-screen flex-col items-center justify-center overflow-x-hidden p-12">
+    <main className="relative flex min-h-screen flex-col items-center justify-around overflow-x-hidden p-12">
+      <div className="absolute top-0 z-[-1] h-2/3 w-full rounded-xl bg-blue-200 shadow-md"></div>
       <div className="flex w-full flex-col items-center">
-        <h1>About Us</h1>
-        <h2>Yap</h2>
+        <h1 className="text-edge-outline cursor-default whitespace-nowrap bg-clip-text px-0.5 py-3.5 font-display text-4xl font-bold sm:text-6xl md:text-8xl">
+          About Us
+        </h1>
+        <h2 className="text-xl">Yap</h2>
       </div>
-      <div className="flex h-96 w-full flex-row gap-4">
+      <div className="flex h-96 w-full flex-row gap-8">
         <PersonCard person={peopleObject.Person1} />
         <PersonCard person={peopleObject.Person2} />
       </div>
-      <div>Others</div>
+      {/* <div>Others</div> */}
     </main>
   );
 }
--- src/components/about/PersonCard.tsx
+++ src/components/about/PersonCard.tsx
@@ -1,3 +1,8 @@
+import Image from "next/image";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+
+import placeholder from "@/src/components/about/placeholder.png";
+
 interface PersonCardProps {
   person: {
     name: string;
@@ -13,27 +18,37 @@ export default function PersonCard({
   person: { name, title, linkedin, github, instagram, message },
 }: PersonCardProps) {
   return (
-    <div className="w-1/2 rounded-xl bg-slate-500 p-8">
+    <div className="w-1/2 rounded-xl bg-white p-8 shadow-md">
       <div className="flex flex-row justify-between">
-        <div className="size-40 rounded-full bg-red-500"></div>
-        <div className="flex flex-col justify-center">
-          <h2>{name}</h2>
-          <p>{title}</p>
+        <div className="flex gap-4">
+          <Image
+            style={{
+              objectFit: "cover",
+              borderRadius: "50%",
+              width: "150px",
+              height: "150px",
+            }}
+            src={placeholder}
+            alt="profile picture"
+          />
+          <div className="flex flex-col justify-center">
+            <h2 className="text-4xl font-bold">{name}</h2>
+            <p className="text-xl">{title}</p>
+          </div>
         </div>
-        <div className="flex flex-col justify-center gap-2">
-          <div className="size-8 rounded-2xl bg-blue-200">
-            <a href={linkedin}></a>
-          </div>
-          <div className="size-8 rounded-2xl bg-blue-200">
-            <a href={github}></a>
-          </div>
-          <div className="size-8 rounded-2xl bg-blue-200">
-            <a href={instagram}></a>
-          </div>
+
+        <div className="flex flex-col justify-center gap-1">
+          <FaInstagram className="size-8" />
+          <a href={instagram}></a>
+          <FaGithub className="size-8" />
+          <a href={github}></a>
+          <FaLinkedin className="size-8" />
+          <a href={linkedin}></a>
         </div>
       </div>
-      <div className="w-full pt-6">
-        <p>{message}</p>
+      <div className="mt-4 h-px w-full bg-zinc-400" />
+      <div className="w-full pt-4">
+        <p className="text-sm">{message}</p>
       </div>
     </div>
   );
